d411003ab0eb062a9f459d32d23f76d1cb553f90
Matthias <mail@matthiasbock.net>
2014-07-07 10:48:57 +0000

    Base64 encoded queries

--- resolve.php
+++ resolve.php
@@ -1,14 +1,36 @@
 <?php
 
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+
 // http://www.php.net/manual/en/function.get-headers.php
 
-$url = $_GET['q'];
+if (array_key_exists('q',$_GET)) {
+	$url = $_GET['q'];
+} else if (array_key_exists('b', $_GET)) {
+	$url = base64_decode($_GET['b']);
+} else {
+	return '""';
+}
+
+//echo $url;
 $headers = get_headers($url,1); 
 
-// http://stackoverflow.com/questions/7064391/php-returning-json-to-jquery-ajax-call
+$loc = $headers['Location'];
 
-header('Content-Type: application/json');
-echo json_encode( $headers['Location'] );
+// if link starts later in line, e.g. /goto.php?url=http://redirectedto.com/
+$i = strpos($loc,"http");
+if ($i > 0) {
+	$loc = substr($loc, $i);
+}
+
+// if is link quoted
+if (substr($loc, 0, 7) == "http%3A") {
+	$loc = urldecode($loc);
+}
+
+// http://stackoverflow.com/questions/7064391/php-returning-json-to-jquery-ajax-call
+echo json_encode($loc);
 
 ?>
 
